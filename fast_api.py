be85a9bc6baccebf29887a893829e83e99aae4f3
Adil ABBADI <adil.abbadi.1996@gmail.com>
2025-03-19 21:49:12 +0000

    fast api stream for ai done

--- fast_api.py
+++ fast_api.py
@@ -1,3 +1,4 @@
+import asyncio
 import time
 import os
 
@@ -6,6 +7,7 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from dotenv import load_dotenv
 from fastapi.responses import StreamingResponse
+from typing import List
 
 load_dotenv()
 
@@ -15,25 +17,34 @@ app = FastAPI()
 
 
 class PromptRequest(BaseModel):
-    prompt: str
+    prompt: List[dict]
 
 @app.post("/stream/")
 async def generate_response(request: PromptRequest):
+    
+    _data =[]
+
+    for j in  request.prompt:
+        element = {}
+        element['role'] = j['role']
+        element['content'] = ""
+
+        if isinstance(j['content'], str):
+            element['content'] = j['content']
+        else:
+            for c in j['content']:
+                if c['type'] == "text":
+                    element['content'] += c['text']
+
+        if  element['content']:
+            _data.append(element)
+    print("Received data ::: " , _data)
     try:
         client = Groq(api_key=groq_api_key)
 
         completion = client.chat.completions.create(
         model="llama3-70b-8192",
-        messages=[
-            {
-                "role": "system",
-                "content": f"You are an assistant"
-            },
-            {
-                "role": "user",
-                "content": f"{request.prompt}"
-            }
-        ],
+        messages=_data,
         temperature=1,
         max_tokens=1024*1,
         top_p=1,
@@ -41,14 +52,14 @@ async def generate_response(request: PromptRequest):
         stop=None,
         )
 
-        def response_stream():
+        async def response_stream():
             for chunk in completion:
-                time.sleep(0.1)
                 content = chunk.choices[0].delta.content
-                if content is not None:
-                    yield content
+                if content:
+                    yield content + "\n"
+                # await asyncio.sleep(5) 
 
-        return StreamingResponse(response_stream(), media_type="text/plain")
+        return StreamingResponse(response_stream(), media_type="text/event-stream")
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
